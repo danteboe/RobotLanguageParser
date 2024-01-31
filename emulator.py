29f4c301b8672428ac84855a4a809e2d24f91765
Juanes2424 <55508909+Juanes2424@users.noreply.github.com>
2024-01-31 18:17:36 -0500

    Update emulator.py

--- emulator.py
+++ emulator.py
@@ -1,1 +1,31 @@
 grid = []
+
+variableDefinitions = {}
+functionDefinitions = {}
+
+
+def createVariable(varName: str, value: any) -> bool:
+    if varName in variableDefinitions:
+        return False
+
+    variableDefinitions[varName] = value
+
+    return True
+
+
+def modifyVariable(varName: str, newValue: any) -> bool:
+    if varName in variableDefinitions:
+        variableDefinitions[varName] = newValue
+
+        return True
+
+    return False
+
+
+def createFunction(funcName: str, value: any) -> bool:
+    if funcName in functionDefinitions:
+        return False
+
+    functionDefinitions[funcName] = value
+
+    return True
